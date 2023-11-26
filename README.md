98e75fc70634dd1a3057683d5e4feef191c41214
perwriters <perwriters@gmail.com>
2023-11-26 04:54:28 +0300

    completed the Movie APP

---
+++ README.md
@@ -0,0 +1,7 @@
+In this Project I recreated the Movie App youtube project and learned on how to use different hooks
+UseState
+useEffect
+I am getting te data from an OMDB api and usng the mivies data to create the app
+the useState was use to handle the state of movies
+while use effect help us to reset the search when the intial serach mounts
+I also used props to in the movie card
---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./Search.svg";
+import Moviecard from "./Moviecard";
+const API_URL = "http://www.omdbapi.com?apikey=c032e2d7";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setsearchTerm] = useState([]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search); // You can do something with the data here
+  };
+  useEffect(() => {}, []);
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      searchMovies(searchTerm);
+    }
+  };
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for Movie"
+          value={searchTerm}
+          onChange={(e) => setsearchTerm(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <Moviecard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
